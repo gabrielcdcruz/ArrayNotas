49c3011b71d95e18636737eca791070239299957
Gabriel Cruz <gabscruz93@gmail.com>
2024-06-22 16:12:32 -0300

    Versão 01

---
+++ src/App.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        double[] notasAluno = new double[n];
+        System.out.println("Quantas nota deseja informar: ");
+        int n = sc.nextInt();
+
+        System.out.println("------------------------------");
+
+        for(int i=0; i<notasAluno.length;i++){
+            System.out.printf("Digite %d nota do aluno: ",n+1);
+            notasAluno[i]=sc.nextDouble();
+        }
+        double soma =0;
+        for(double notasMedia: notasAluno){
+            soma=+ notasAluno[];
+        }
+
+    }
+}
