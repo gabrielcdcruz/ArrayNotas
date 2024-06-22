b2a8b6dc39a10b68592d38a9112ddb12137731b2
Gabriel Cruz <gabscruz93@gmail.com>
2024-06-22 16:40:10 -0300

    Versao 02 corrigida

--- src/App.java
+++ src/App.java
@@ -1,24 +1,29 @@
 import java.util.Arrays;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
 
-        double[] notasAluno = new double[n];
         System.out.println("Quantas nota deseja informar: ");
         int n = sc.nextInt();
+        double[] notasAluno = new double[n];
 
         System.out.println("------------------------------");
 
-        for(int i=0; i<notasAluno.length;i++){
-            System.out.printf("Digite %d nota do aluno: ",n+1);
-            notasAluno[i]=sc.nextDouble();
+        for (int i = 0; i < notasAluno.length; i++) {
+            System.out.printf("Digite %d nota do aluno: ", i + 1);
+            notasAluno[i] = sc.nextDouble();
         }
-        double soma =0;
-        for(double notasMedia: notasAluno){
-            soma=+ notasAluno[];
+
+        System.out.println("------------------------------");
+        double soma = 0.0;
+        for (double notas : notasAluno) {
+            soma += notas;
         }
+        double media = soma / notasAluno.length;
+        System.out.printf("A media do aluno é de: %.2f", media);
 
     }
 }
